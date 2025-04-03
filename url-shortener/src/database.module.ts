233845739a0e4538b1945b0584e1f019cd88ecf1
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2025-04-03 16:07:06 +0530

    added documentation

--- url-shortener/src/database.module.ts
+++ url-shortener/src/database.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         host: 'localhost',
         port: 3306,
         username: 'root',
-        password: 'REDACTED', 
+        // password: 'your_password', 
         database: 'nest_url_shortener',
         autoLoadEntities: true,
         synchronize: true,
